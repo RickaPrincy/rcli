9c751692edf72ba34536b4933626fc78245c6f07
RickaPrincy <rckprincy@gmail.com>
2023-11-27 21:51:07 +0300

    feat: ask value in options implemented

--- example/main.cpp
+++ example/main.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]){
-    if(RCli::get_boolean("Play chess ? ", false))
-        std::cout << "ok" << std::endl;
-    else
-        std::cout << "tsy ok" << std::endl;
-    return 0;
+    std::cout << RCli::ask_value_in_options("Project type",
+    { "library", "executable" }) << std::endl;
 }
