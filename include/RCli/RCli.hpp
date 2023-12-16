ed82b7925510e8e43f306a59936f084944cb85ad
RickaPrincy <rckprincy@gmail.com>
2023-12-16 18:29:31 +0300

    chore: put app in other file

--- include/RCli/RCli.hpp
+++ include/RCli/RCli.hpp
@@ -4,14 +4,6 @@
     #include <RCli/types.hpp>
     
     namespace RCli{
-        class App{
-            private:
-                RCli::String  name, version, description;
-            public:
-                App(RCli::String name, RCli::String version, RCli::String description);
-                void print_version();
-        };
-
         RCli::String ask_input_value(OptionValue option);
         RCli::VectorString ask_inputs_values(std::vector<OptionValue> options);
         String ask_value_in_list(OptionValue config, VectorString options, bool ignore_case = false);
