accc5d3fd845a8085d143a18cccb39e932a37e59
RickaPrincy <rckprincy@gmail.com>
2023-12-25 16:56:05 +0300

    chore: auto download deps

--- configure_and_build.sh
+++ configure_and_build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cmake -DRCLI_VERSION_BUILD=ON -S . -B build; cd build; make; cd ..
+cmake -S . -B build; cd build; make; cd ..
--- dev_install.sh
+++ dev_install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sudo cmake -DRCLI_VERSION_BUILD=ON -DRCLI_INSTALL=ON -S . -B build
+sudo cmake -DRCLI_INSTALL=ON -S . -B build
 cd build; make install; cd ..
