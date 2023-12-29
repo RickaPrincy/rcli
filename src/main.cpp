0ad8ea87434da9416aadabd62a3db2e24924c18e
RickaPrincy <rckprincy@gmail.com>
2023-12-29 22:04:19 +0300

    chore: avoid include external

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "RCli.h"
 #include "RCli/add.h"
 #include <iostream>
-#include "external/TColor/TColor.hpp"
+#include "TColor/TColor.hpp"
 
 int RCli::add(int a, int b){
     TColor::write_endl(TColor::B_BLUE, "Hello");
