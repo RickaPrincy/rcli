9cc2868adda7dd1446507d698cd6dc38301d4662
RickaPrincy <rckprincy@gmail.com>
2024-01-13 12:11:15 +0300

    refactor: configure clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <rcli/rcli.hpp>
 #include <iostream>
+#include <rcli/rcli.hpp>
 
-void rcli::sayHello(std::string name){
-    std::cout << "Hello " << name << std::endl;;
+void rcli::sayHello(std::string name) {
+    std::cout << "Hello " << name << std::endl;
 }
---
+++ src/utils/utils.hpp
@@ -0,0 +1,6 @@
+#ifndef __RCLI_UTILS__
+#define __RCLI_UTILS__
+    namespace rcli{
+        
+    }
+#endif // !__RCLI_UTILS__
