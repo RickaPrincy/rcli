b0c8340d1273b97a7b35ee9b81cda6cedc2d20c1
RickaPrincy <rckprincy@gmail.com>
2024-01-13 12:54:55 +0300

    refactor: rewrite utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <rcli/rcli.hpp>
+#include "utils/utils.hpp" 
 
 void rcli::sayHello(std::string name) {
-    std::cout << "Hello " << name << std::endl;
+    std::vector<int> test{1,4,7};
+    std::cout << rcli::join(test, "-") << std::endl;
 }
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -1,6 +1,21 @@
 #ifndef __RCLI_UTILS__
 #define __RCLI_UTILS__
-    namespace rcli{
-        
+
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace rcli {
+    template <typename T>
+    std::string join(const T &values, std::string separator) {
+        return std::accumulate(values.begin(), values.end(), std::to_string(*values.begin()),
+            [&separator](const auto &a, const auto &b) {
+                std::ostringstream oss;
+                oss << a << separator << b;
+                return oss.str();
+            });
     }
-#endif // !__RCLI_UTILS__
+}  // namespace rcli
+
+#endif  // !__RCLI_UTILS__
