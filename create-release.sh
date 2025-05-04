3f5f5aecca21ea14482effdd65410eee40881810
RickaPrincy <rckprincy@gmail.com>
2025-05-04 08:38:24 +0300

    release: rcli@2.0.2

--- create-release.sh
+++ create-release.sh
@@ -23,7 +23,7 @@ create_tar_release(){
 
 create_lib_release(){
 
-    local RELEASE_NAME=rcli@2.0.1
+    local RELEASE_NAME=rcli@2.0.2
     release_path+=("${RELEASE_NAME}")
     
     mkdir -p "${RELEASE_NAME}"
@@ -41,5 +41,5 @@ create_lib_release(){
 create_lib_release
 
 git add --all
-git commit -m "release: rcli@2.0.1"
-git tag -a v2.0.1 -m "rcli v2.0.1"
+git commit -m "release: rcli@2.0.2"
+git tag -a v2.0.2 -m "rcli v2.0.2"
